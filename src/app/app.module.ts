548d4b3e43fd83de7e8626a81a88d2b79310cc42
Bougarraschawki <bougarraschawki1998@gmail.com>
2023-05-10 10:55:07 +0100

    mon premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { StoreModule } from './store/store.module';
+import { AppComponent } from './app.component';
+import { CheckoutComponent } from './store/checkout/checkout.component';
+import {CartDetailComponent } from './store/cartDetail/cartDetail.component';
+import { StoreComponent } from './store/store.component';
+import { StoreFirstGuard } from './storeFirst.guard';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    RouterModule.forRoot([
+      {path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard]},
+      {path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard]},
+      {path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard]},
+      {path: '**', redirectTo: '/store'},
+
+    ]),
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
+import { CartSummaryComponent } from "./cartSummary/cartSummary.component";
+import { CartDetailComponent } from "./cartDetail/cartDetail.component";
+import { CheckoutComponent } from "./checkout/checkout.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-    imports: [ ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
-    exports: [StoreComponent]
+    imports: [ ModelModule, BrowserModule, FormsModule, RouterModule],
+    declarations: [ StoreComponent, 
+                    CartSummaryComponent,
+                    CartDetailComponent,
+                    CheckoutComponent
+                ],
+
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 
 export class StoreModule {}
